89c2fddd56df8999b621543ec39002e5213d0f4c
SHEFFcode <sheff@sheffmachine.com>
2017-03-14 22:59:35 -0700

    add regions, and look into strings vs stringbuilder

--- CollectionsVsArrays/Program.cs
+++ CollectionsVsArrays/Program.cs
@@ -10,43 +10,63 @@ namespace CollectionsVsArrays
     {
         static void Main(string[] args)
         {
-//            //Create a collection
-//            var dogs = new List<string>();
-//            dogs.Add("Bulldog");
-//            dogs.Add("Collie");
-//            dogs.Add("Retriever");
-//            //use foreach to move through these
-//            foreach (var dog in dogs)
-//            {
-//                Console.WriteLine(dog + " ");
-//            }
-//
-//            Console.WriteLine(dogs[0]);
-//
-//            //Create an array
-//            string[] dogsArray = new string[] {"Bulldog", "Collie", "Retriever"};
-//            foreach (var dog in dogsArray)
-//            {
-//                Console.WriteLine(dog);
-//            }
+            #region Array vs Collection
+            //            //Create a collection
+            //            var dogs = new List<string>();
+            //            dogs.Add("Bulldog");
+            //            dogs.Add("Collie");
+            //            dogs.Add("Retriever");
+            //            //use foreach to move through these
+            //            foreach (var dog in dogs)
+            //            {
+            //                Console.WriteLine(dog + " ");
+            //            }
+            //
+            //            Console.WriteLine(dogs[0]);
+            //
+            //            //Create an array
+            //            string[] dogsArray = new string[] {"Bulldog", "Collie", "Retriever"};
+            //            foreach (var dog in dogsArray)
+            //            {
+            //                Console.WriteLine(dog);
+            //            }
 
-            //playing around with exceptions
-            int a = 4;
-            int b = 0;
-            try
-            {
-                Console.WriteLine($"a divided by b is {a/b}.");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine($"You cannot divide by 0. Exception is {ex}");
-            }
-            finally
-            {
-                Console.WriteLine("This gets called regardless.");
-            }
 
-            
+            #endregion
+
+
+            #region Exceptions
+            //            //playing around with exceptions
+            //            int a = 4;
+            //            int b = 0;
+            //            try
+            //            {
+            //                Console.WriteLine($"a divided by b is {a/b}.");
+            //            }
+            //            catch (DivideByZeroException ex)
+            //            {
+            //                Console.WriteLine($"You cannot divide by 0. Exception is {ex}");
+            //            }
+            //            finally
+            //            {
+            //                Console.WriteLine("This gets called regardless.");
+            //            }
+
+
+            #endregion
+
+            #region String vs StringBuilder
+
+            string myString = "This is an example of ";
+            myString = myString + "concactination."; //original mystring is destroyed and a new one was created with concated info.
+            Console.WriteLine(myString);
+
+            #endregion
+
+
+
+
+
         }
     }
 }
